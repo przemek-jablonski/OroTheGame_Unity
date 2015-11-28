c2475e90afa949c3f310a4d5b25f078c72e3b85d
Przemysław Jabłoński <sharaquss@gmail.com>
2015-11-28 22:36:45 +0100

    destroyable box startHealth problem

--- Assets/Scripts/InheritancePattern/DestructablePropScript.cs
+++ Assets/Scripts/InheritancePattern/DestructablePropScript.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DestructablePropScript : MonoBehaviour {
+public class DestructablePropScript : OroLivingActor {
+
+	//new public float startHealth = 20;
+	
+	public GameObject lootPouch;
+	
 
 	// Use this for initialization
-	void Start () {
-	
+	public override void Start () {
+		base.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	public override void HitBehaviour() {
+		// none
+	}
+	
+	public override void DeathBehaviour() {
+		// spawn some wooden planks
+		// spawn loot
+		// spawn some smoke effect
+		// spawn particle on loot (loot sparkling) but not here anyway
 	}
 }
