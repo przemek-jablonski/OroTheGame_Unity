4b282900e89a2f9fc8e2a90ccbba11dd15d9bd72
ciemek <sharaquss@gmail.com>
2016-05-19 21:21:32 +0200

    creating and initially randomizing terrain works

--- Assets/Modules/IndoorMapGeneration/Scripts/TerrainIndoorGenerator.cs
+++ Assets/Modules/IndoorMapGeneration/Scripts/TerrainIndoorGenerator.cs
@@ -1,90 +1,109 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using UnityEditor;
+using AssemblyCSharp;
 
 public class TerrainIndoorGenerator : MonoBehaviour {
 
-      //max value of total x coordinates
-      public int 		terrainWidth = 150;
-      //max value of total y coordinates
-      public int		terrainDepth = 150;
-    //max value of total z coordinates
-    public int terrainHeight = 1;
+	//max value of total x coordinates
+	public int 		terrainWidthX = 150;
+	//max value of total y coordinates
+	public int		terrainDepthY = 150;
+	//max value of total z coordinates
+	public int 		terrainHeightZ = 1;
 
-    [RangeAttribute(0, 1)]
-      public float         fillPercent = 0.4f;
+	[RangeAttribute(0, 1)]
+	public float  	fillPercent = 0.45f;
 
-      public int          iterationCount = 4;
-      public string 	randomSeed = "sample seed";
-      public bool 	      randomizeSeed = true;
-
-
-      //todo: logic module shouldnt be here i suppose?
-      bool[,] terrain;
-      System.Random random;
+	public int    	iterationCount = 4;
+	public string 	randomSeed = "sample seed";
+	public bool 	randomizeSeed = true;
 
 
-      //todo: refactor to be able to generate terrain inside editor as well as in-game
+	//todo: logic module shouldnt be here i suppose?
+	bool[,] terrain;
+	System.Random random;
 
 
-      public void Start() {
-            CreateMap();
-            FillMapRandom();
-            
-            if (randomizeSeed) {
-                  RandomizeSeed();
-            }
-
-            random = new System.Random(randomSeed.GetHashCode());
-      }
-      
-      public void Update() {
-            if (Input.anyKey) {
-                  CellularAutomataIteration();
-            }
-      }
-      
-      private void CreateMap() {
-            terrain = new bool[terrainWidth, terrainHeight];
-      }
-      
-      private void FillMapRandom() { 
-            for (int x = 0; x < terrainWidth; x++) {
-                  for (int y = 0; y < terrainHeight; y++) {
-                        
-                  }
-            }
-            
-      }
-      
-      private void FillMapPerlinNoise() {
-            //...
-      }
-      
-      private void FillMapSimplex() {
-            //...
-      }
-      
-      private void CellularAutomataIteration() {
-            
-      }
+	//todo: refactor to be able to generate terrain inside editor as well as in-game
 
 
-      private void RandomizeSeed() {
-            randomSeed = (DateTime.Now.Ticks * (UnityEngine.Random.value +1)).ToString();
-      }
+	public void Start() {
+		random = new System.Random(randomSeed.GetHashCode());
+		if (randomizeSeed) { 
+			RandomizeSeed();
+		}
 
-      
-      public void OnValidate() {
-                  if (terrainWidth < 10)
-                        terrainWidth = 10;
-                  if (terrainHeight < 10)
-                        terrainHeight = 10;
-                  if (iterationCount < 1)
-                        iterationCount = 1;
-                  if (randomSeed == string.Empty)
-                        randomSeed = "sample seed";
-      }
-            
+		CreateMap();
+		FillMapRandom();
+	}
+
+	public void Update() {
+		if (Input.anyKey) {
+		      CellularAutomataIteration();
+		}
+	}
+
+	private void CreateMap() {
+		terrain = new bool[terrainWidthX, terrainDepthY];
+	}
+
+	private void FillMapRandom() { 
+		for (int x = 0; x < terrainWidthX; x++) {
+		      for (int y = 0; y < terrainDepthY; y++) {
+				if (x == 0 || y == 0 || x == terrainWidthX - 1 || y == terrainDepthY - 1) {
+					terrain [x, y] = true;
+				} else {
+					terrain [x, y] = Utils.randomBool (fillPercent);
+				}
+		    }
+		}
+
+	}
+
+	private void FillMapPerlinNoise() {
+		//...
+	}
+
+	private void FillMapSimplex() {
+		//...
+	}
+
+	private void CellularAutomataIteration() {
+
+	}
+
+
+	private void RandomizeSeed() {
+		randomSeed = (DateTime.Now.Ticks * (UnityEngine.Random.value +1)).ToString();
+	}
+
+
+	public void OnValidate() {
+		if (terrainWidthX < 10)
+		    terrainWidthX = 10;
+		if (terrainDepthY < 10)
+			terrainDepthY = 10;
+		if (terrainHeightZ != 1)
+		    terrainHeightZ = 1;
+		if (iterationCount < 1)
+		    iterationCount = 1;
+		if (randomSeed == string.Empty)
+		    randomSeed = "sample seed";
+	}
+
+	void OnDrawGizmos() {
+		//todo: exception here instead of this check?
+		if (terrain != null) {
+			for (int x = 0; x < terrainWidthX; x ++) {
+				for (int y = 0; y < terrainDepthY; y ++) {
+					Gizmos.color = (terrain[x,y] == true)?Color.black:Color.white;
+					Vector3 pos = new Vector3(-terrainWidthX/2 + x + .5f,0, -terrainDepthY/2 + y+.5f);
+					Gizmos.DrawCube(pos,Vector3.one);
+				}
+			}
+		}
+	}
 
 }
---
+++ Assets/Modules/IndoorMapGeneration/Scripts/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AssemblyCSharp {
+	
+	public static class Utils {
+
+		static System.Random random;
+
+		static Utils() {
+			random = new System.Random ();
+		}
+
+		/**
+		 * returns bool, chance of getting true is proportional to percentChance
+		 * percentChance is an integer in range (0, 100)
+		 **/
+		public static bool randomBool(int percentChance) {
+			if (random.Next (0, 100) < percentChance)
+				return true;
+			return false;
+		}
+
+		/**
+		 * returns bool, chance of getting true is proportional to percentChance
+		 * percentChance is a floating point number in range (0.0, 1.0)
+		 **/
+		public static bool randomBool(float percentChance) {
+			if (random.NextDouble () < percentChance)
+				return true;
+			return false;
+		}
+
+	}
+
+}
+
